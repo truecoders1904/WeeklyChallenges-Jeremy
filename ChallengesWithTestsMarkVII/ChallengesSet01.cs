8758a39baaac8f319f3800cef8e6c7795bbb1394
Jeremy <jlong.223.jl@gmail.com>
2019-04-17 00:17:28 -0500

    added some changes to the bool method if two numbers equal

--- ChallengesWithTestsMarkVII/ChallengesSet01.cs
+++ ChallengesWithTestsMarkVII/ChallengesSet01.cs
@@ -8,7 +8,7 @@ namespace ChallengesWithTestsMarkVII
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            throw new NotImplementedException();
+            return (num1 == num2);          
         }
 
         public double Subtract(double minuend, double subtrahend)
