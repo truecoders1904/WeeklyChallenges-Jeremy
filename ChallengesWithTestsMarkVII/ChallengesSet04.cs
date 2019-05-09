d1b45b5898c4d2cbba6c97973fbd037531700851
Jeremy <jlong.223.jl@gmail.com>
2019-05-08 23:28:15 -0500

    added changes to the CouldFormTriangle, GetLength, and ChangeBusinessName methods

--- ChallengesWithTestsMarkVII/ChallengesSet04.cs
+++ ChallengesWithTestsMarkVII/ChallengesSet04.cs
@@ -26,7 +26,16 @@ namespace ChallengesWithTestsMarkVII
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+
+            int length1 = str1.Length;
+            int length2 = str2.Length;
+            int length3 = str3.Length;
+            int length4 = str4.Length;
+
+            List<int> lengthlist = new List<int> { length1, length2, length3, length4 };
+
+
+            return lengthlist.Min();
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
@@ -39,12 +48,13 @@ namespace ChallengesWithTestsMarkVII
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            return (sideLength1 + sideLength2 > sideLength3) && (sideLength3 + sideLength2 > sideLength1)
+                && (sideLength3 + sideLength1 > sideLength2);
         }
 
         public bool IsStringANumber(string input)
